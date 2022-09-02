4aa9a1ef00d925d01f5dfa29efc27f2931de717f
Joseph Bursey <jbursey@uci.edu>
2022-09-02 14:19:37 -0700

    Tools: actually add that tool

---
+++ tools/findmerge.cpp
@@ -0,0 +1,156 @@
+#include <date.h>
+#include <argparse.h>
+#include <bug_info.h>
+#include <file_api.h>
+#include <consts.h>
+#include <git_api.h>
+#include <version.h>
+
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <git2.h>
+
+using namespace std;
+
+int main(int argc, char ** argv)
+{
+    int id, err = 0, k;
+    string find_hash, guilty_hash, 
+           linux_repo_remote, logfilename,
+           kernel_commit_name = "";
+    Bug_Info bug;
+    Argparse args;
+    Version merge_commit;
+    git_repository *linux_repo = nullptr;
+    int git_err;
+
+    ofstream logfile;
+    vector<Version> kernel_versions;
+
+    args.expect("FGih");
+    args.expect(vector<string>({ "help" }));
+    args.parse(argc, argv);
+    if (args.is_set('h') || args.is_set("help"))
+    {
+        cout << "Help:\n"
+            << "    -F [find_hash]: the hash of the finding commit.\n"
+            << "    -G [guilty_hash]: the hash of the guilty commit.\n"
+            << "    -i [id]: REQUIRED. The id of the inspector.\n"
+            << endl;
+        return 0;
+    }
+
+    git_libgit2_init();
+
+    if (args.is_set('F'))
+        find_hash = args.get_arg_as_string('F');
+    else
+    {
+        cout << "Error: No finding commit given. Please use -F [hash].\n";
+        goto finish;
+    }
+
+    if (args.is_set('G'))
+        guilty_hash = args.get_arg_as_string('G');
+    else
+    {
+        cout << "Error: No guilty commit given. Please use -G [hash].\n";
+        goto finish;
+    }
+
+    if (args.is_set('i'))
+        id = args.get_arg_as_int('i');
+    else
+    {
+        cout << "Error: No id given. Please use -i [id]\n";
+        err = -1;
+        goto finish;
+    }
+
+    // get information about the bug
+    bug.parse_config_file("../wd-inspector-" + to_string(id) + "/" + "bug.cfg");
+
+    if (!check_file(bug.get_wd() + "/log"))
+        make_dir(bug.get_wd() + "/log");
+
+    // Set up linux repository locally
+    linux_repo_remote = LINUX_REPO_REMOTE + bug.get_repo();
+
+    if (!check_file(bug.get_kerneldir()))
+        make_dir(bug.get_kerneldir());
+
+    // this may cause issues when the repository opened is not the one we want
+    git_err = git_repository_open(&linux_repo, bug.get_kerneldir().c_str());
+    if (git_err < 0)
+    {
+        cout << "Cloning Linux repository...\n";
+        git_err = git_clone(&linux_repo, linux_repo_remote.c_str(), bug.get_kerneldir().c_str(), nullptr);
+        if (git_err < 0)
+        {
+            cerr << "Error: Git clone failed.\n";
+            err = -1;
+            goto finish;
+        }
+    }
+
+    // begin logging
+    logfilename = bug.get_wd() + "/log/merges.log";
+    logfile.open(logfilename, ios_base::app);
+    if(!logfile)
+    {
+        cerr << "Error: Failed to open log file " << logfilename << ".\n";
+        err = -1;
+        goto finish;
+    }
+
+    // commits are arranged newest (low index) to oldest (high index)
+    cout << SPACER
+         << "Gathering kernel versions...\n";
+    kernel_versions = get_kernel_versions(bug, guilty_hash, find_hash);
+    cout << "Found " << kernel_versions.size() << " kernel commits.\n";
+    if (kernel_versions.size() == 0)
+    {
+        logfile << "Error: Failed to gather kernel versions.\n" << flush;
+        goto finish;
+    }
+    
+    //  ======================================================================================================
+    // Find Merge Commit
+
+    cout << SPACER
+         << "Looking for Merge Commit...\n";
+
+    merge_commit = git_find_merge_commit(bug.get_kerneldir(), kernel_versions, guilty_hash);
+
+    if (!merge_commit.name.empty())
+    {
+        cout << "Merge commit found: " << merge_commit.name << ".\n";
+        logfile << "Bug " << bug.get_number() << ": " << merge_commit.date.get_date() << " - " << merge_commit.name << ".\n" << flush;
+    }
+    else
+    {
+        cout << "No merge commit found.\n";
+        logfile << "Bug " << bug.get_number() << ": No Merge Commit.\n" << flush;
+    }
+
+    // ======================================================================================================
+    // Finish
+    // ======================================================================================================
+
+finish:
+    if (logfile)
+    {
+        logfile << flush;
+        logfile.close();
+    }
+
+    if (linux_repo)
+        git_repository_free(linux_repo);
+    
+    git_libgit2_shutdown();
+    cout << "Done.\n" << flush;
+    return err;
+}
---
+++ tools/findmerges-shell.sh
@@ -0,0 +1,175 @@
+#!/bin/bash
+
+set -e
+
+source ../parameters/config.cfg
+
+# input file. Made by parse/bparse.sh
+bugfile=$inspectdir/parse/bugs.csv
+
+# the date that syzbot started fuzzing
+syzbotDate="2017-07-22"
+
+# number to identify the bug by
+number=1
+wd=""
+startLine=1
+endLine=0
+
+# =================================================================================================
+# Functions
+
+writebugconfig () {
+    echo -n "" > $inspectorconfig
+    echo "curBug=\"$curBug\"" >> $inspectorconfig
+    echo "bugname=\"$bugName\"" >> $inspectorconfig
+    echo "kpref=\"$kpref\"" >> $inspectorconfig
+    echo "arch=$arch" >> $inspectorconfig
+    echo "repo=$repo" >> $inspectorconfig
+    echo "kconfig=$inspectdir/$wd/config-$curBug.txt" >> $inspectorconfig
+    echo "repro=$inspectdir/$wd/repro-$curBug.prog" >> $inspectorconfig
+    echo "kallerwd=$inspectdir/$wd/wd-kaller" >> $inspectorconfig
+    echo "syzconfig=$inspectdir/$wd/syzkaller.cfg" >> $inspectorconfig
+    echo "managerwd=$inspectdir/$wd" >> $inspectorconfig
+    echo "syzdir=$inspectdir/$wd/syzkaller" >> $inspectorconfig
+    echo "buglink=\"$buglink\"" >> $inspectorconfig
+}
+
+printhelp () {
+    echo "Help:"
+    echo "    s - the line in parse/bugs.csv to start on"
+    echo "    e - the last line in parse/bugs.csv to use"
+    echo "    i - the id given to this manager"
+    echo "    b - determine the name of the bug file in parse/"
+}
+
+# =================================================================================================
+
+setuponly=""
+nopoc=""
+mtime=""
+findonly=""
+
+# get the start and end lines from the arguments
+while getopts "s:e:i:b:" flag
+do
+    case $flag in
+        s)
+            startLine="${OPTARG}" ;;
+        e)
+            endLine="${OPTARG}" ;;
+        i)
+            id="${OPTARG}" ;;
+        b)
+            bugfile="$inspectdir/parse/${OPTARG}" ;;
+        *)
+            printhelp
+            exit
+    esac
+done
+
+if (( endLine == 0 )); then
+    endLine=$(( $(cat $bugfile | wc -l) ))
+fi
+
+if [[ $id == "" ]]; then
+    echo "No id given. Use -i <id>"
+    exit
+fi
+
+wd="wd-inspector-$id"
+inspectorconfig=../$wd/bug.cfg
+logfile=../$wd/log/merges.log
+
+if [ ! -d ../$wd ]; then
+    echo "Making the working directory: ../$wd"
+    mkdir ../$wd
+fi
+
+if [ ! -d ../$wd/log ]; then
+    echo "Making the log directory"
+    mkdir ../$wd/log
+fi
+
+totallines=$(cat $bugfile | wc -l)
+if (( $endLine > $totallines || $startLine < 1 || $startLine > $endLine )); then
+    echo "Bad start and/or end lines!"
+    exit
+fi
+
+line=$startLine
+
+echo "file: $bugfile startline: $startLine endline: $endLine" >> $logfile
+echo "" >> $logfile
+
+while (( $line <= $endLine )); do
+    linetext=$(sed -n ${line}p $bugfile)
+    fixDate=$(echo "$linetext" | awk -F',' '{ print $5; }')
+    findDate=$(echo "$linetext" | awk -F',' '{ print $9; }')
+    guiltyDate=$(echo "$linetext" | awk -F',' '{ print $11; }')
+
+    arch=$(echo "$linetext" | awk -F',' '{ print $12; }')
+
+    # if the bug is older than syzbot, use syzbot as the starting date
+    syzbotAge=$(( $($inspectdir/helpers/diffdate $guiltyDate $syzbotDate) ))
+    if (( $syzbotAge < 0 )); then
+        startDate=$syzbotDate
+    else
+        startDate=$guiltyDate
+    fi
+
+    # check that the fixing date is not bogus
+    fixAge=$(( $($inspectdir/helpers/diffdate $fixDate $findDate) ))
+    if (( $fixAge < 0 )); then
+        # sed at the end because shell script is weird about what is escaped in urls...
+        findlink=$(echo "$linetext" | awk -F',' '{ print $8; }' | sed 's/\\//' | sed 's/log/commit/')
+        snapshot=$(lynx -dump -dont_wrap_pre -width=300 $findlink)
+        findDate=$(echo "$snapshot" | grep -m 1 "^[ ]*committer " | grep -o "20[0-9][0-9]-[0-9][0-9]-[0-9][0-9]" | cat)
+        fixAge=$(( $($inspectdir/helpers/diffdate $fixDate $findDate) ))
+    fi
+
+    # check that the time to find is good and interesting
+    findAge=$(( $($inspectdir/helpers/diffdate $findDate $startDate) ))
+
+    if (( $findAge > 1 && $fixAge >= 0 )) && [[ $arch == "amd64" ]]; then
+        # bug number and name
+        curBug="bug$line"
+        bugName="$(echo "$linetext" | awk -F',' '{ print $2; }')"
+        configlink="$(echo "$linetext" | awk -F',' '{ print $7; }')"
+        reprolink=$(echo "$linetext" | awk -F',' '{ print $6; }')
+
+        # linux kernel repository
+        # the link to the finding commit has what we need
+        findlink=$(echo "$linetext" | awk -F',' '{ print $8; }')
+        repo=$(echo "$findlink" | grep "https://git.kernel" | awk -F'/' '{ print $9"/"$10; }' | cat)
+        if [[ $repo == "torvalds/linux.git" ]]; then
+            kpref="linux"
+        else
+            echo "Bad repository on line $line: $repo"
+            kpref=""
+        fi
+
+        guiltylink=$(echo "$linetext" | awk -F',' '{ print $10; }')
+
+        # clean up the bug name if it is a duplicate name
+        if [[ $(echo "$bugName" | grep "([0-9]*)$" | cat) != "" ]]; then
+            bugName=${bugName::-4}
+        fi
+
+        findhash=$(echo $findlink | grep -o "[0-9a-f]*$" | cat)
+        buglink=$(echo "$linetext" | awk -F',' '{ print $1; }')
+        guiltyhash=$(echo $guiltylink | grep -o "[0-9a-f]*$" | cat)
+
+        if [[ $reprolink != "" && $configlink != "" && $bugName != "" && $findlink != "" && $repo != "" && $kpref != "" ]]; then
+            writebugconfig
+            set +e
+            ./findmerge -F $findhash -G $guiltyhash -i $id
+            set -e
+            number=$(( $number + 1 ))
+        fi
+    fi
+
+    line=$(( $line + 1 ))
+done
+
+echo "All Done!"
