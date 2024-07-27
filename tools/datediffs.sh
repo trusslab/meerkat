f7c52362570003f1b6aa208737004ede5f59ef5c
Joseph Bursey <jbursey@uci.edu>
2024-07-27 13:07:38 -0700

    Update datediffs shellscript

--- tools/datediffs.sh
+++ tools/datediffs.sh
@@ -2,20 +2,26 @@
 
 set -e
 
+bugfile=parse/bugs-2023-unique.csv
+
 finding=()
 guilty=()
 
-# get all of the finding dates
-readarray -t finding <<< "$(awk -F',' '{ print $7 }' < bugs.csv)"
+which helpers/diffdate
+if (( $? != 0 )); then
+    echo "Could not find helpers/diffdate"
+fi
 
+# get all of the finding dates
+readarray -t finding <<< "$(awk -F',' '{ print $8 }' < ${bugfile})"
 
 # get all of the guilty dates
-readarray -t guilty <<< "$(awk -F',' '{ print $9 }' < bugs.csv)"
+readarray -t guilty <<< "$(awk -F',' '{ print $3 }' < ${bugfile})"
 
 for (( i=1; i<${#finding[@]}; i++ )); do
-    d=$(( $(../helpers/diffdate ${finding[$i]} ${guilty[$i]}) ))
+    d=$(( $(helpers/diffdate ${finding[$i]} ${guilty[$i]}) ))
 #    l=$(( $(../helpers/diffdate ${guilty[$i]} "2017-09-26") ))
-    if (( $d > 0 )); then
+    if (( $d >= 0 )); then
         echo "${finding[$i]},${guilty[$i]},$d"
     fi
 done
