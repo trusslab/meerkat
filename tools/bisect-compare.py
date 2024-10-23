3d053e446a8bde6f22e6d10ba5c00009a95e102f
Joseph Bursey <jbursey@uci.edu>
2024-10-22 17:10:01 -0700

    tools/bisect-compare: progress

--- tools/bisect-compare.py
+++ tools/bisect-compare.py
@@ -1,4 +1,5 @@
 import os
+from datetime import date
 from urllib.request import urlopen
 from lxml import html
 
@@ -10,8 +11,8 @@ from lxml import html
         # parse bisect log?
 # compare to actual introducing commit (bugfile)
 
-bugfilename = ""
-outfilename = ""
+bugfilename = "../results/batch1/bugs-batch1.csv"
+outfilename = "bisect-comp.log"
 
 def fetch_link(l):
     rawpage = urlopen(l)
@@ -25,14 +26,41 @@ def main():
     outfile = open(outfilename, 'w')
     for line in buglines:
         buglink = line.split(',')[0]
+        print(line.split(',')[1])
+        print(buglink)
         bughtml = fetch_link(buglink)
-        # body/div/div<class = bug-bisection-info>
-        # b/text() contains Cause Bisection
-            # for fail, b/text() contains "failed"
-            # for success, b/text() contains "introduced by"
-        # span<class = mono>/text() contains:
-            # "commit <commit>"
-            # "Date: <date>"
+        bisect_result = bughtml.xpath("//body/div/div[@class='bug-bisection-info']/b[1]/text()")
+        if (len(bisect_result) == 0):
+            print("No result found")
+            print()
+            continue
+        if ("Cause bisection:" not in bisect_result[0]):
+            print("Failed to parse bisection result: " + bisect_result[0])
+            print()
+            continue
+        
+        if ("introduced by" in bisect_result[0]):
+            print("Found good result")
+            kcommit_link = bughtml.xpath("//body/div/div[@class='bug-bisection-info']/span[@class='mono']/a/@href")
+            if (len(kcommit_link) == 0):
+                print("Failed to find bisect commit link")
+                print()
+                continue
+            kcommit_link = kcommit_link[0]
+            print(kcommit_link)
+            kcommit_html = fetch_link(kcommit_link)
+            kcommit_dates = kcommit_html.xpath("//body/div[@id='cgit']/div[@class='content']/table[@class='commit-info']/tr/td[@class='right']/text()")
+            if (len(kcommit_dates) < 2):
+                print("Failed to find bisect commit dates")
+                print()
+                continue
+            bisect_date = date.fromisoformat(kcommit_dates[0][:10])
+            print(bisect_date.isoformat())
+
+        elif ("failed" in bisect_result[0]):
+            print("Found bisect error")
+            # go fetch the error log and print it
+        print()
 
     outfile.close()
     os._exit(os.EX_OK)
