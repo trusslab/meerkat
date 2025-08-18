6b6cda84cdc31529c656c0018fa4d0d1e127537d
Joseph Bursey <jbursey@uci.edu>
2025-08-18 12:25:08 -0700

    Remove unused functions

--- include/version.h
+++ include/version.h
@@ -6,8 +6,6 @@
 #include <string>
 #include <vector>
 
-#define STABLE_INFER_SIZE 50
-
 // A class to store information about versions of 
 // gcc, linux, or syzkaller (or anything else).
 // Stores an identifying value and the assiciated
@@ -37,28 +35,5 @@ public:
     bool operator==(const Version &);
     bool operator!=(const Version &);
 };
-
-class Version_p
-{
-public:
-    Version v;
-    std::vector<std::string> parents;
-};
-
-void infer_stability(std::vector<Version> &, bool = false);
-std::vector<Version> get_only_stable(const std::vector<Version> &);
-
-Version get_version_by_date(const std::vector<Version> &, const Date &);
-Version get_stable_version_by_date(const std::vector<Version> &, const Date &);
-
-int get_index_by_id(const std::vector<Version> &, const std::string &, const int pos = 0);
-
-int get_index_by_id(const std::vector<Version_p> &, const std::string &, const int pos = 0);
-
-// get the index of the first (oldest) version on or after the given date
-int get_starting_index(const std::vector<Version> &, const Date &);
-
-// get the index of the last (most recent) version on or before the given date
-int get_ending_index(const std::vector<Version> &, const Date &);
 
 #endif
