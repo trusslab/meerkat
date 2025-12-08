dcd99a9c47aa52a1fee562cb779e5c1d4462e508
Joseph Bursey <jbursey@uci.edu>
2025-12-08 12:24:35 -0800

    Try a fix for a disabled syscall

--- lib/template_parse.cpp
+++ lib/template_parse.cpp
@@ -529,6 +529,9 @@ void push_syscall_depends(std::vector<Syscall> &syscalls, int index, std::vector
         {"bpf$TOKEN_CREATE", {TypeTag(resourceClass, "fd_bpf_prog")}},
         {"bpf$BPF_PROG_DETACH", {TypeTag(resourceClass, "fd_cgroup"), TypeTag(resourceClass, "fd_bpf_link")}},
         {"bpf$BPF_LINK_CREATE_XDP", {TypeTag(syscallClass, "bpf$LINK_GET_FD_BY_ID")}},
+        {"nanosleep", {TypeTag(resourceClass, "timespec")}},
+        {"timer_settime", {TypeTag(resourceClass, "timespec")}},
+        {"utimes", {TypeTag(resourceClass, "timespec")}},
     };
 
     if (problematic.count(syscalls.at(index).get_name()) == 1)
