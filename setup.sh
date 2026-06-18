8b4a261c6167144f1d2bc8dd961cd8c26a7ee115
Ocelot <57074600+jtbursey@users.noreply.github.com>
2026-06-18 00:18:17 -0700

    setup.sh: only do it if you need to

--- setup.sh
+++ setup.sh
@@ -42,6 +42,8 @@ fi
 if [ ! -d gcc-10.1.0 ]; then
     log "Unpacking Compilers"
     tar -xzf compilers.tar.gz
+fi
+if [ ! -L /usr/lib/x86_64-linux-gnu/libmpfr.so.4 ]; then
     log "*** READ THIS ***"
     log "Alright, the solution for a missing library is to manually link it to the newer version."
     log "The command to be run is: sudo ln -s /usr/lib/x86_64-linux-gnu/libmpfr.so.6 /usr/lib/x86_64-linux-gnu/libmpfr.so.4"
